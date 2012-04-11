353b5f916f5e8732b995de646d8307a52f308eae
plewiv <plewiv@cox.net>
2012-04-10 19:51:48 -0700

    Remember Me done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,20 @@ class SessionsController < ApplicationController
   def create
    user = User.authenticate(params[:session][:email],
                              params[:session][:password])
+	remember = params[:session][:remember]
     if user.nil?
       flash.now[:error] = "Invalid email/password combination."
       @title = "Sign in"
       render 'new'
     else
-      sign_in user
-      redirect_to user
+       if (params[:remember]=="1")
+          flash[:success] = "Confirmed, Will remember login."
+          perm_sign_in user
+		else
+		flash[:success] = "Will not remember login."
+          temp_sign_in user
+		end
+		redirect_to user
     end
   end
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,12 @@
 module SessionsHelper
 
-  def sign_in(user)
-    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
-    self.current_user = user
+  def temp_sign_in(user)
+	cookies.signed[:remember_token] = [user.id, user.salt]
+	self.current_user = user
+  end
+  def perm_sign_in(user)
+	cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+	self.current_user = user
   end
   def current_user=(user)
     @current_user = user
